8b04d3349a6c0430a72801f65ac3b0b2c648ea08
dmuller104 <dmuller104@gmail.com>
2022-09-07 01:18:35 -0600

    Remove index as row in data

--- conglomerate_data.py
+++ conglomerate_data.py
@@ -3,9 +3,12 @@ positions = ('DB','DL','DST','K','LB','QB','RB','TE','WR')
 import os
 import pandas as pd
 
+src_dir = "raw_data"
+target_dir = "data"
+
 for position in positions:
-    players = os.listdir(position)
-    player_dfs = (pd.read_csv(f'{position}/{player}') for player in players)
+    players = os.listdir(f"{src_dir}/{position}")
+    player_dfs = (pd.read_csv(f'{src_dir}/{position}/{player}') for player in players)
     position_df = pd.concat(player_dfs,ignore_index=True)
-    position_df.to_csv(f'data/{position}.csv')
+    position_df.to_csv(f'data/{position}.csv',index=False)
 
